a1ba0c3b154dfbd725646d8703d23dc47e0d61b3
NockXu <gabriel.dav@outlook.fr>
2025-02-06 21:51:09 +0100

    Add BlockController

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,12 +1,12 @@
-import minecraftData from 'minecraft-data';
 import express, { Request, Response, NextFunction } from 'express';
-
-const mcData = minecraftData('1.21')
+import { BlockController } from './Block/Block.route';
 
 export const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(BlockController);
 
 app.use((req: Request, _res: Response, next : NextFunction) => {
   const timestamp = new Date().toISOString();
