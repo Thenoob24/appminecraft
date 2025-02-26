f485891a68efce04aa7d22e6af7859f0cb577341
garniik <antoine.arnoultfr@gmail.com>
2025-02-26 16:20:37 +0100

    image

--- app_minecraft/lib/widgets/image.dart
+++ app_minecraft/lib/widgets/image.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Image extends StatelessWidget {
-  const Image(required )
+class CustomImage extends StatelessWidget {
+  const CustomImage({super.key, required this.nom});
+
+  final String nom;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("lib/images/items/$nom.png",width: 75,
+      height: 75,fit:BoxFit.fill );
+  }
 }
-
-
