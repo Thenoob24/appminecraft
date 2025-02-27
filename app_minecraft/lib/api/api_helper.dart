5a8f54385bd0da444231c18c4115d6348957a379
NockXu <gabriel.dav@outlook.fr>
2025-02-27 15:13:39 +0100

    ajout lien API

--- app_minecraft/lib/api/api_helper.dart
+++ app_minecraft/lib/api/api_helper.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../dio/dio_provider.dart';
 
@@ -11,33 +12,21 @@ class ApiHelper {
   ApiHelper({required this.dio});
   final Dio dio;
   final List<String> version = [
-    "0.30c",
     "1.7",
     "1.8",
-    "15w40b",
     "1.9",
-    "1.9.1-pre2",
     "1.9.2",
     "1.9.4",
-    "16w20a",
-    "1.10-pre1",
     "1.10",
     "1.10.1",
     "1.10.2",
-    "16w35a",
     "1.11",
     "1.11.2",
-    "17w15a",
-    "17w18b",
-    "1.12-pre4",
     "1.12",
     "1.12.1",
     "1.12.2",
-    "17w50a",
     "1.13",
     "1.13.1",
-    "1.13.2-pre1",
-    "1.13.2-pre2",
     "1.13.2",
     "1.14",
     "1.14.1",
@@ -46,15 +35,11 @@ class ApiHelper {
     "1.15",
     "1.15.1",
     "1.15.2",
-    "20w13b",
-    "20w14a",
-    "1.16-rc1",
     "1.16",
     "1.16.1",
     "1.16.2",
     "1.16.3",
     "1.16.4",
-    "21w07a",
     "1.17",
     "1.17.1",
     "1.18",
@@ -75,18 +60,22 @@ class ApiHelper {
     "1.21.3",
     "1.21.4"
   ];
+  dynamic data;
 
-  void get(String version, String searchQuery) async {
-    final url = 'http:/62.72.18.63:5713/$version/blocks';
+  void get(String searchQuery) async {
+    final url = 'http:/62.72.18.63:5713/items';
 
     try {
       final response = await dio.get(url, queryParameters: {
         'search': searchQuery,
       });
-      print('Response status: ${response.statusCode}');
-      print('Response data: ${response.data}');
+
+      data = response.data;
+
     } catch (e) {
-      print("Error fetching data: $e");
+      if (kDebugMode) {
+        print("Error fetching data: $e");
+      }
     }
   }
 }
